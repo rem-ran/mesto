390f466494cc992447ad9c372e54eaf2a57dbff6
rem.ran <rem.ran@yandex.com>
2022-09-04 21:31:10 +0300

    refactore code for BEM

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,8 @@ let popup = document.querySelector(".popup");
 let closeProfEditBtn = popup.querySelector(".popup__close-btn");
 let popupContainer = popup.querySelector(".popup__container");
 let popupSaveBtn = popup.querySelector(".popup__save-btn");
-let inputName = popup.querySelector(".input__txt_type_name");
-let inputProfession = popup.querySelector(".input__txt_type_profession");
+let inputName = popup.querySelector(".input-txt_type_name");
+let inputProfession = popup.querySelector(".input-txt_type_profession");
 
 let profile = document.querySelector(".profile");
 let openProfEditBtn = profile.querySelector(".profile__edit-btn");
@@ -33,5 +33,15 @@ function formSubmitHandler (evt) {
    openCloseEditPopup();
 }
 
+function handleKeyPress(e){
+   let key = e.keyCode || e.which;
+    if (key === 13) {
+      profileName.textContent = inputName.value ;
+      profileProfession.textContent = inputProfession.value;
+      openCloseEditPopup();
+    }
+  }
+
 popupContainer.addEventListener('submit', formSubmitHandler);
 popupSaveBtn.addEventListener('click', formSubmitHandler);
+
